28b7f04f8426171ba05742b03e613622aad47108
Adrian H <adrian@tollyx.net>
2016-10-11 00:04:33 +0200

    Fixed roulette command

--- plugins/roulette.py
+++ plugins/roulette.py
@@ -1,5 +1,6 @@
 import asyncio
 import random
+import discord
 
 async def on_command(client, message, args):
     if message.channel.is_private: return
@@ -7,10 +8,10 @@ async def on_command(client, message, args):
         if message.author.server_permissions.manage_channels:
             participants = []
             for member in message.server.members:
-                if member.status == 'online' and not member.bot:
-                    perms = message.channel.permissions_for(member)
-                    if perms.read_messages:
-                        participants.append(member)
-            roll = random.randint(0, len(participants))
+                if member.status != discord.Status.online: continue
+                perms = message.channel.permissions_for(member)
+                if perms.read_messages:
+                    participants.append(member)
+            roll = random.randint(0, len(participants)-1)
             winner = participants[roll]
             await client.send_message(message.channel, "And the winner is: " + winner.mention + "!")
